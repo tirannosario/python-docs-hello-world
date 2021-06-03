0ab694fe5c5553f4db963cde902cde6e2164b95e
tirannosario <glt.rosario@gmail.com>
2021-06-03 16:44:52 +0200

    modificata app e locustfile

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask, session, redirect, request
+import os
+
+server_port = 5000
 app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY') 
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
+    return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect("/")
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect("/")
 
 @app.route("/<user>")
 def show_user(user):
-    return "Hello %s from Rosario :)" % user
+    return "This is the profile of %s" % user
+
+
+@app.route("/<user>/friends")
+def show_user_friends(user):
+    return "These are the %s's friends: <br> Obi Wan <br> Darth Vader <br> Grogu" % user
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', port = server_port)
--- locustfile.py
+++ locustfile.py
@@ -1,20 +1,27 @@
 from locust import HttpUser, task, between
+import time
 
 class WebsiteTestUser(HttpUser):
     wait_time = between(0.5, 2.0)
     
     def on_start(self):
-        """ on_start is called when a Locust start before any task is scheduled """
+        self.client.post("/login", {"username":"locust-user"})
         pass
 
     def on_stop(self):
-        """ on_stop is called when the TaskSet is stopping """
+        self.client.get("/logout")
         pass
 
-    @task(3)
-    def hello_world(self):
+    @task(4)
+    def test_index(self):
         self.client.get("/")
 
-    @task(2)
-    def hello_user(self):
+    @task(3)
+    def test_user_profile(self):
         self.client.get("/Locust")
+
+    @task(2)
+    def test_user_friends(self):
+        self.client.get("/Locust")
+        time.sleep(2)
+        self.client.get("/Locust/friends")
